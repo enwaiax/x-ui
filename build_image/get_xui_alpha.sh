70483321a982f1a9a49b06d14233f4d642f10918
LuckyHunter <32839114+Chasing66@users.noreply.github.com>
2022-12-22 02:51:22 +0000

    Update script

--- build_image/get_xui_alpha.sh
+++ build_image/get_xui_alpha.sh
@@ -2,32 +2,7 @@
 
 red='\033[0;31m'
 green='\033[0;32m'
-yellow='\033[0;33m'
 plain='\033[0m'
-
-# check root
-[[ $EUID -ne 0 ]] && echo -e "${red}Error: ${plain} must be root to run this script! \n" && exit 1
-
-# check os
-if [[ -f /etc/redhat-release ]]; then
-    release="centos"
-elif cat /etc/issue | grep -Eqi "debian"; then
-    release="debian"
-elif cat /etc/issue | grep -Eqi "armbian"; then
-    release="armbian"
-elif cat /etc/issue | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-elif cat /proc/version | grep -Eqi "debian"; then
-    release="debian"
-elif cat /proc/version | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-else
-    echo -e "System version not detected by ${red}, please contact the script author! ${plain}\n" && exit 1
-fi
 
 arch=$(arch)
 
@@ -43,28 +18,6 @@ fi
 
 echo "Your CPU arch: ${arch}"
 
-# os version
-if [[ -f /etc/os-release ]]; then
-    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
-fi
-if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
-    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
-fi
-
-if [[ "${release}" == "centos" ]]; then
-    if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}Please use CentOS 7 or higher! ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "ubuntu" ]]; then
-    if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}Please use Ubuntu 16 or later! ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "debian" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}Please use Debian 8 or higher!${plain}\n" && exit 1
-    fi
-fi
-
 install_x-ui() {
     mkdir -p /go
     cd /go || exit
@@ -76,13 +29,13 @@ install_x-ui() {
     fi
     echo -e "x-ui version: ${last_version}, start installation"
     wget -q -N --no-check-certificate \
-        -O x-ui-linux-${arch}.tar.gz \
-        https://github.com/FranzKafkaYu/x-ui/releases/download/"${last_version}"/x-ui-linux-${arch}.tar.gz
+        -O x-ui-linux-"${arch}".tar.gz \
+        https://github.com/FranzKafkaYu/x-ui/releases/download/"${last_version}"/x-ui-linux-"${arch}"-english.tar.gz
     if [[ $? -ne 0 ]]; then
         echo -e "${red}Failed to download x-ui${plain}"
         exit 1
     fi
-    tar zxvf x-ui-linux-${arch}.tar.gz
+    tar zxvf x-ui-linux-"${arch}".tar.gz
     chmod +x x-ui/x-ui
 }
 
--- build_image/get_xui_beta.sh
+++ build_image/get_xui_beta.sh
@@ -2,7 +2,6 @@
 
 red='\033[0;31m'
 green='\033[0;32m'
-yellow='\033[0;33m'
 plain='\033[0m'
 
 arch=$(arch)
